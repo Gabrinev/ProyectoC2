295d6b9f1f47815a251c9243c110e509e772b712
Ignasi Elgström <i.elgstrom@gmail.com>
2022-02-25 13:45:31 +0100

    control de modificar correo para + apuntes

---
+++ functions.js
@@ -0,0 +1,14 @@
+function ModifyCorreo () {
+$("#defaultCheck1").click(function(){
+    if (this.checked){
+    $("#staticEmail").attr("disabled", false)
+}
+    else{
+        $("#staticEmail").attr("disabled", true)
+
+    }
+    return;
+}
+)
+return;
+};
