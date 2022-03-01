d10b4804c84e4aef33566ae3e7325ad2cc139bf7
Ignasi <i.elgstrom@gmail.com>
2022-03-02 00:10:09 +0100

    more changes

--- functions.js
+++ functions.js
@@ -1,41 +1,20 @@
-
-function ModifyCorreo () {
-$("#defaultCheck1").click(function(){
-    if (this.checked){
-    $("#staticEmail").attr("disabled", false)
-}
-    else{
-        $("#staticEmail").attr("disabled", true)
-
-    }
-    return;
-}
-)
-return;
-};
-
 /*Funciones de la Entrada a cargo de Josuè Martín*/
-function Comprobar() 
-{
-    var UsuariInsertat = $("#UsuariInput").val()
-    var ContraInsertat = $("#ContraInput").val()
-    console.log("Hola")   
-    if(UsuariInsertat === "Pepito" && ContraInsertat === "123456X")
-    {
-        document.cookie = "UsuariInsertat=Pepito; Contrasenya=123456X";
-        window.location.href = "/home.html"
-    }
-	if(UsuariInsertat != "Pepito" && ContraInsertat != "123456X")
-    {
-        alert("Nom d'usuari o contrasenya incorrectes")
-    }
-    if(UsuariInsertat === "Pepito" && ContraInsertat != "123456X")
-    {
-        alert("Contrasenya incorrecta")
-    }
+function Comprobar() {
+  var UsuariInsertat = $("#UsuariInput").val();
+  var ContraInsertat = $("#ContraInput").val();
+  console.log("Hola");
+  if (UsuariInsertat === "Pepito" && ContraInsertat === "123456X") {
+    document.cookie = "UsuariInsertat=Pepito; Contrasenya=123456X";
+    window.location.href = "/home.html";
+  }
+  if (UsuariInsertat != "Pepito" && ContraInsertat != "123456X") {
+    alert("Nom d'usuari o contrasenya incorrectes");
+  }
+  if (UsuariInsertat === "Pepito" && ContraInsertat != "123456X") {
+    alert("Contrasenya incorrecta");
+  }
 }
 
-function Redirigir() 
-{
-    window.location.href = "/registro.html"
+function Redirigir() {
+  window.location.href = "/registro.html";
 }
--- validar.js
+++ validar.js
@@ -1,52 +1,52 @@
-function validar(){
-   var nombre,apellidos,correo,usuario,clave,telefono,expresion;
-   nombre = document.getElementById("nombre").value;
-   apellidos = document.getElementById("apellidos").value;
-   correo = document.getElementById("correo").value;
-   usuario = document.getElementById("usuario").value;
-   clave = document.getElementById("clave").value;
-   telefono = document.getElementById("telefono").value;
-   expresion = /^([da-z_.-]+)@([da-z.-]+).([a-z.]{2,6})$/;
+function validar() {
+  var nombre, apellidos, correo, usuario, clave, telefono, expresion;
+  nombre = document.getElementById("nombre").value;
+  apellidos = document.getElementById("apellidos").value;
+  correo = document.getElementById("correo").value;
+  usuario = document.getElementById("usuario").value;
+  clave = document.getElementById("clave").value;
+  telefono = document.getElementById("telefono").value;
+  expresion = /^([da-z_.-]+)@([da-z.-]+).([a-z.]{2,6})$/;
 
-   if(nombre === "" || apellidos === "" || correo === "" || usuario === "" || clave === "" || telefono === ""){
-       alert("todos los campos son obligatorios");
-       return false;
-   }
-
-   else if(nombre.length>30){
+  if (
+    nombre === "" ||
+    apellidos === "" ||
+    correo === "" ||
+    usuario === "" ||
+    clave === "" ||
+    telefono === ""
+  ) {
+    alert("todos los campos son obligatorios");
+    return false;
+  } else if (nombre.length > 30) {
     alert("nombre demasiado largo");
     return false;
-   }
-
-   else if(apellidos.length>80){
+  } else if (apellidos.length > 80) {
     alert("los apellidos son muy largos");
     return false;
-   }
-   else if(correo.length>100){
+  } else if (correo.length > 100) {
     alert("El correo es muy largo");
     return false;
-}
-else if(!expresion.test(correo)){
+  } else if (!expresion.test(correo)) {
     alert("El correo no es valido");
     return false;
-}
-else if(usuario.length>29 || clave.length>20){
+  } else if (usuario.length > 29 || clave.length > 20) {
     alert("El usuario y la clave solo debe tener 20 cadracteres como maximo");
     return false;
-}
-else if(telefono.length>10){
+  } else if (telefono.length > 10) {
     alert("El telefono es muy largo");
     return false;
-}
-
-else if(isNaN(telefono)){
+  } else if (isNaN(telefono)) {
     alert("El telefono ingresando no es un número");
     return false;
-}
- function Entrar(){
-    
-    if(nombre.value !=0 || apellidos.value !=0 || correo.value !=0 ){
-        window.location.href="/Entrar.html";
+  }
+
+  document.cookie = `Usuario=${nombre}`;
+  window.location.href = "./Entrar.html";
+
+  function Entrar() {
+    if (nombre.value != 0 || apellidos.value != 0 || correo.value != 0) {
+      window.location.href = "/Entrar.html";
     }
- }
+  }
 }
