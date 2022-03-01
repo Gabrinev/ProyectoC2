5736a14a9da1f0079d3125c863876f18e6d31825
Ignasi <i.elgstrom@gmail.com>
2022-03-02 00:17:14 +0100

    routing correction

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ function Comprobar() {
   console.log("Hola");
   if (UsuariInsertat === "Pepito" && ContraInsertat === "123456X") {
     document.cookie = "UsuariInsertat=Pepito; Contrasenya=123456X";
-    window.location.href = "/home.html";
+    window.location.href = "./home.html";
   }
   if (UsuariInsertat != "Pepito" && ContraInsertat != "123456X") {
     alert("Nom d'usuari o contrasenya incorrectes");
