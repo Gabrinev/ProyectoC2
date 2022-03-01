fdd87d9a5957845655c059c5f43c7411ff32400d
Josue <josuemartintb@gmail.com>
2022-03-01 11:36:04 +0100

    Cambios Entrada y redireccion

--- functions.js
+++ functions.js
@@ -22,12 +22,19 @@ function Comprobar()
     console.log("Hola")
     if(UsuariInsertat === "Pepito" && ContraInsertat === "123456X")
     {
-        alert("Continui")
+        window.location.href = "/home.html"
     }
-    else
+	if(UsuariInsertat != "Pepito" && ContraInsertat != "123456X")
     {
         alert("Nom d'usuari o contrasenya incorrectes")
     }
-   
+    if(UsuariInsertat === "Pepito" && ContraInsertat != "123456X")
+    {
+        alert("Contrasenya incorrecta")
+    }
 }
 
+function Redirigir() 
+{
+    window.location.href = "/SignIn.html"
+}
