ab177a4b8b5d9e8f15fab37962e491c9419f29aa
Tony Gomez <Tonygomez9817@gmail.com>
2022-03-02 10:47:02 +0100

    cambios del registro, y funciones

--- functions.js
+++ functions.js
@@ -5,16 +5,16 @@ function validar() {
   nombre = document.getElementById("nombre").value;
   apellidos = document.getElementById("apellidos").value;
   correo = document.getElementById("correo").value;
-  usuario = document.getElementById("usuario").value;
-  clave = document.getElementById("clave").value;
+  clave = document.getElementById("clave1").value;
   telefono = document.getElementById("telefono").value;
   expresion = /^([da-z_.-]+)@([da-z.-]+).([a-z.]{2,6})$/;
+  var password1 = document.getElementById("clave1").value;
+  var password2 = document.getElementById("clave2").value;
 
   if (
     nombre === "" ||
     apellidos === "" ||
     correo === "" ||
-    usuario === "" ||
     clave === "" ||
     telefono === ""
   ) {
@@ -32,18 +32,18 @@ function validar() {
   } else if (expresion.test(correo)) {
     alert("El correo no es valido");
     return false;
-  } else if (usuario.length > 29 || clave.length > 20) {
-    alert("El usuario y la clave solo debe tener 20 cadracteres como maximo");
-    return false;
-  } else if (telefono.length > 10) {
+  } else if (telefono.length != 9) {
     alert("El telefono es muy largo");
     return false;
   } else if (isNaN(telefono)) {
     alert("El telefono ingresando no es un número");
     return false;
+  } else if (password1 != password2) {
+    alert("Las contraseñas no coinciden");
+    return false;
   }
 
-  document.cookie = `Usuario=${usuario}`;
+  document.cookie = `Email=${usuario}`;
   document.cookie = `Contraseña=${clave}`;
   window.location.href = "./home.html";
 }
@@ -56,15 +56,13 @@ function Comprobar() {
   var UsuariInsertat = $("#UsuariInput").val();
   var ContraInsertat = $("#ContraInput").val();
   expresion = /^([da-z_.-]+)@([da-z.-]+).([a-z.]{2,6})$/;
-  if (expresion.test(UsuariInsertat) && UsuariInsertat === "Pepito@gmail.com" && ContraInsertat === "123456X") 
-  {   
+  if (expresion.test(UsuariInsertat) && UsuariInsertat === "Pepito@gmail.com" && ContraInsertat === "123456X") {
     window.location.href = "./home.html";
   }
-  else
-  {
+  else {
     alert("Nom d'usuari o contrasenya incorrectes");
   }
- 
+
 }
 
 function Redirigir() {
