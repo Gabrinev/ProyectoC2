b7f7405dfad6eb295002571db96c16a4196a2292
Halley <halleylambertp@gmail.com>
2022-03-02 11:07:34 +0100

    Cambios gmail

--- functions.js
+++ functions.js
@@ -55,8 +55,7 @@ function validar() {
 function Comprobar() {
   var UsuariInsertat = $("#UsuariInput").val();
   var ContraInsertat = $("#ContraInput").val();
-  expresion = /^([da-z_.-]+)@([da-z.-]+).([a-z.]{2,6})$/;
-  if (expresion.test(UsuariInsertat) && UsuariInsertat === "pepito@gmail.com" && ContraInsertat === "123456X") 
+  if (UsuariInsertat === "pepito@gmail.com" && ContraInsertat === "123456X") 
   {   
     window.location.href = "./home.html";
   }
