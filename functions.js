44a31fec572b463e6bc61c306da1681e12e172fa
Gabriele <gabriele.nevini@gmail.com>
2022-03-02 11:44:42 +0100

    funciones por 111 vez

--- functions.js
+++ functions.js
@@ -5,10 +5,11 @@ function validar() {
   nombre = document.getElementById("nombre").value;
   apellidos = document.getElementById("apellidos").value;
   correo = document.getElementById("correo").value;
-  usuario = document.getElementById("usuario").value;
-  clave = document.getElementById("clave").value;
+  clave = document.getElementById("clave1").value;
   telefono = document.getElementById("telefono").value;
   expresion = /^([da-z_.-]+)@([da-z.-]+).([a-z.]{2,6})$/;
+  var contra1 = document.getElementById("clave1").value;
+  var contra2 = document.getElementById("clave2").value;
 
   if (
     nombre === "" ||
@@ -20,30 +21,30 @@ function validar() {
   ) {
     alert("todos los campos son obligatorios");
     return false;
-  } else if (nombre.length > 30) {
+  } else if (nombre.length > 23) {
     alert("nombre demasiado largo");
     return false;
-  } else if (apellidos.length > 80) {
+  } else if (apellidos.length > 23) {
     alert("los apellidos son muy largos");
     return false;
   } else if (correo.length > 100) {
     alert("El correo es muy largo");
     return false;
-  } else if (expresion.test(correo)) {
+  } else if (!expresion.test(correo)) {
     alert("El correo no es valido");
     return false;
-  } else if (usuario.length > 29 || clave.length > 20) {
-    alert("El usuario y la clave solo debe tener 20 cadracteres como maximo");
-    return false;
-  } else if (telefono.length > 10) {
+  } else if (telefono.length != 9) {
     alert("El telefono es muy largo");
     return false;
   } else if (isNaN(telefono)) {
     alert("El telefono ingresando no es un número");
     return false;
+  } else if (contra1 != contra2){
+    alert("Las contraseñas no coinciden");
+    return false;
   }
 
-  document.cookie = `Usuario=${usuario}`;
+  document.cookie = `Email=${correo}`;
   document.cookie = `Contraseña=${clave}`;
   window.location.href = "./home.html";
 }
